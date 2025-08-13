e0179f25c72a45dce5bc12b69ce319b099c3c09b
Sebastian <pocztasebastian1306@gmail.com>
2025-08-13 17:40:23 +0200

    nav bar mała rozdzielczosc

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,7 @@
 const toggle = document.querySelector('#menu-ikon');
 const nav = document.querySelector('nav');
 
+//po kliknięciu na element #menu-ikon zmień element nav na klasę vertical-nav i przelączaj się między nimi
 toggle.addEventListener('click', () => {
   nav.classList.toggle ('vertical-nav');
 });
